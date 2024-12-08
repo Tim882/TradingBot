db0119b16912188d7d757f14e4d4a5c7c1f3e0e6
Sharf <sharf_ti@mail.ru>
2024-12-08 10:22:44 +0300

    Added simple rabbit event publishing

---
+++ TradingBot/InputLoopService.cs
@@ -0,0 +1,47 @@
+﻿using Messages;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingBot
+{
+    public class InputLoopService(IMessageSession messageSession) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (true)
+            {
+                Console.WriteLine("Press 'P' to place an order, or 'Q' to quit.");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.P:
+                        // Instantiate the command
+                        var positionEvent = new OpenedPosition
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Ticker = "SBER"
+                        };
+
+                        // Send the event
+                        Console.WriteLine($"Sending PlaceOrder command, OrderId = {positionEvent.Id}");
+                        await messageSession.Publish(positionEvent, stoppingToken);
+
+                        break;
+
+                    case ConsoleKey.Q:
+                        return;
+
+                    default:
+                        Console.WriteLine("Unknown input. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
--- TradingBot/Program.cs
+++ TradingBot/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Messages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using NServiceBus.Logging;
+using TradingBot;
+
+Console.Title = "Trading bot";
+
+var endpointConfiguration = new EndpointConfiguration("Rabbit.TradingBot");
+endpointConfiguration.UseSerialization<SystemJsonSerializer>();
+endpointConfiguration.EnableInstallers();
+var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+transport.ConnectionString("host=localhost");
+transport.UseDirectRoutingTopology(QueueType.Classic);
+
+var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+
+await RunLoop(endpointInstance);
+
+await endpointInstance.Stop().ConfigureAwait(false);
+
+static async Task RunLoop(IEndpointInstance endpointInstance)
+{
+    while (true)
+    {
+        Console.WriteLine("Press 'P' to place an order, or 'Q' to quit.");
+        var key = Console.ReadKey();
+        Console.WriteLine();
+
+        List<string> values = new List<string> { "SBER", "LKOH", "GAZP", "YDEX" };
+
+        Random rnd = new Random();
+
+        switch (key.Key)
+        {
+            case ConsoleKey.P:
+                // Instantiate the command
+                var positionEvent = new OpenedPosition
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Ticker = values[rnd.Next(0, values.Count - 1)]
+                };
+
+                // Send the event
+                Console.WriteLine($"Sending PlaceOrder command, OrderId = {positionEvent.Id}");
+                await endpointInstance.Publish(positionEvent);
+
+                break;
+
+            case ConsoleKey.Q:
+                return;
+
+            default:
+                Console.WriteLine("Unknown input. Please try again.");
+                break;
+        }
+    }
+}
